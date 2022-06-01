454d71aa34b7d60f046914d8c685807327ec4221
kaanakgundogdu <kaanakgundogdu@gmail.com>
2022-06-01 15:56:13 +0300

    Error handling added.Main.py modified.

--- 3-NATO-alphabet/main.py
+++ 3-NATO-alphabet/main.py
@@ -1,3 +1,4 @@
+import pandas
 student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
 
 # Looping through dictionaries:
@@ -5,12 +6,20 @@ for (key, value) in student_dict.items():
     # Access key and value
     pass
 
-import pandas
 
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_dict = {row.letter: row.code for index, row in nato_df.iterrows()}
 
-user_input = input(f"Type a word: ")
 
-for i in user_input:
-    print(f"{nato_dict[i.upper()]} for {i.upper()}")
+def generate_phonetic():
+    user_input = input(f"Type a word: ")
+    try:
+        output_list = [nato_dict[letter.upper()] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(output_list)
+
+
+generate_phonetic()
